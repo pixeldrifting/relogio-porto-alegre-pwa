9ab0764f25665624b101397be270a2242c4d4363
PixelDrift <batista.bg23@gmail.com>
2025-12-18 19:56:24 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,32 @@
-const CACHE = 'relogio-poa-v1';
-const FILES = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+const CACHE_NAME = "relogio-poa-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
 
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(FILES))
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', evt => {
-  evt.waitUntil(self.clients.claim());
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', evt => {
-  // Try network first, fallback to cache
-  evt.respondWith(
-    fetch(evt.request).catch(()=>caches.match(evt.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp =>
+      resp || fetch(event.request)
+    )
   );
 });
